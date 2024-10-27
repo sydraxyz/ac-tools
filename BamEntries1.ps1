9e363e63297e720f6e0fa1b1d1a3b5e2a2d0a2e4
Arda Berk <73230850+sydraxyz@users.noreply.github.com>
2024-10-27 21:53:55 +0300

    Update BamEntries1.ps1

--- BamEntries1.ps1
+++ BamEntries1.ps1
@@ -79,8 +79,8 @@ $Bam = Foreach ($Sid in $Users){$u++
             
         foreach($rp in $rpath){
            $BamItems = Get-Item -Path "$($rp)UserSettings\$Sid" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
-           Write-Host -ForegroundColor Red "Extracting " -NoNewLine
-           Write-Host -ForegroundColor Blue "$($rp)UserSettings\$SID"
+           Write-Host -ForegroundColor Blue "Extracting " -NoNewLine
+           Write-Host -ForegroundColor White "$($rp)UserSettings\$SID"
            $bi = 0 
 
             Try{
